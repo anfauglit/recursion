78964fe2ee45e4d086971ff517299b5df8a0be06
Alex Chekalin <anfauglit@mail.ru>
2021-03-19 20:19:26 +0300

    Permutations

---
+++ perm.py
@@ -0,0 +1,19 @@
+def char_swap(s, n, m):
+	new_s = list(s) 
+	tmp = new_s[n]
+	new_s[n] = new_s[m]
+	new_s[m] = tmp
+	return ''.join(new_s)
+
+def print_perm(s, i):
+	if i == len(s)-1:
+		print(s)
+	else:
+		print_perm(s[:], i+1)
+		for index in range(i+1,len(s)):
+			if s[i] != s[index]:
+				swapped = char_swap(s, i, index)
+				print_perm(swapped, i+1)
+
+mystring = 'ABB' 
+print_perm(mystring, 0)
