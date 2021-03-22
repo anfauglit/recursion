b8f81ce4d8cee057f6d2e3df57ec35e292d49632
Alex <anfauglit@mail.ru>
2021-03-23 02:53:39 +0300

    Farmer solved

--- farmer.py
+++ farmer.py
@@ -1,29 +1,51 @@
-def save_state(a):
-	if len(a) == 0 or len(a) == 1:
-		return True
-	if len(a) == 2 and 'G' not in a:
-		return True
+import numpy as np
 
-def trans(A, B,out):
-	if len(A) == 0:
-		return True
-	if save_state(B):
-		for i in A:
-			new_A = A[:]
-			new_A.remove(i)
-			B.append(i)
-			if trans(B,A,out):
-				out.append(i)
-				return True
+def next_move(c):
+    while True:
+        yield c[0]
+        c = c[1:] + c[:1]
 
-		if trans(B,A,out):
-			out.append('F')
-			return True
-	out.pop()
-	return False
+def finished(s):
+    return 0 not in s.values()
+
+def save_state(s, departure):
+    a = [i for i,v in s.items() if v == departure]
+    if len(a) == 0 or len(a) == 1:
+        return True
+    if len(a) == 2 and 'G' not in a:
+        return True
+
+    return False
+
+def trans(state, moves, departure, out):
+    departure ^= 1
+
+    if finished(state):
+        return True
+
+    for i in range(4): 
+        move = next(moves)
+        if move == 'F':
+            if save_state(state, departure):
+                out.append(move)
+                if trans(state, moves, departure, out):
+                    return True
+                out.pop()
+        elif state[move] == departure:
+            state[move] ^= 1
+            if save_state(state, departure):
+                out.append(move)
+                if trans(state, moves, departure, out):
+                    return True
+                out.pop()
+            state[move] ^= 1
+
+    return False
 
 out = []
-A = list('GCW')
-B = []
-trans(A,B,out)
+a = list('CGW')
+b = list(np.zeros(3, int))
+state = dict(zip(a,b))
+gen = next_move('CGWF')
+trans(state, gen, 1, out)
 print(out)
